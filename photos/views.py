fa76b871e4a4575d65e600f49f8e94a5a2bdc128
nziokaivy <nziokaivy@gmail.com>
2019-03-08 09:10:27 +0300

    add url file to app, connect to project url and add template

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Image, Profile, Comments
 # Create your views here.
+
+def home(request):
+  
+    return render(request, 'index.html')
+
