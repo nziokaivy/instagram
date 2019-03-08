cb2a53b880e1106edda1847540fd434a9e0bdeb5
nziokaivy <nziokaivy@gmail.com>
2019-03-08 10:46:48 +0300

    add registration a nd login functionality

--- photos/views.py
+++ photos/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Image, Profile, Comments
 # Create your views here.
 
+
 def home(request):
   
     return render(request, 'index.html')
