0765f2db158fac50add37c13fefc6cae82790267
nziokaivy <nziokaivy@gmail.com>
2019-03-11 22:21:15 +0300

    add heroku deployed app

--- instagram/settings.py
+++ instagram/settings.py
@@ -160,6 +160,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
